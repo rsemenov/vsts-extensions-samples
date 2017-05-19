68eac1fb752a98d37e6a64f8525199fedef22e1a
Ruslan Semenov <rsemenov@microsoft.com>
2017-05-19 19:39:31 -0400

    add more examples

--- vsts-pr-status-menu-extension/scripts/contributedItems.js
+++ vsts-pr-status-menu-extension/scripts/contributedItems.js
@@ -1,19 +1,53 @@
+const NotSet = 0;
+const Pending = 1;
+const Succeeded = 2;
+const Failed = 3;
+const Error = 4;
+
 VSS.register("sample-pr-status-action-provider", {
     execute: function (actionArgs) {
         alert("execute: " + (JSON.stringify(actionArgs) || "").substr(0, 100));
     },
     getMenuItems: function (context) {
         var menuItems = [];
+
         menuItems.push({
-            title: "Queue release",
-            action: function (actionContext) {
-                alert("action: " + context.status.description);
-            },
-            disabled: false,
-            separator: false,
-            childItems: null,
-            icon: "css://bowtie-icon bowtie-status-run-outline"
+            title: "Settings",
+            icon: "css://bowtie-icon bowtie-settings-wrench",
+            childItems: [
+                {
+                    title: "Update release",
+                    action: function (actionContext) {
+                        alert("Execute action for status " + context.status.context.name);
+                    },
+                },
+                { title: "Update build", disabled: true }
+            ]
         });
+
+        menuItems.push({ title: "-" }); // separator
+
+        if (context.status.state !== Pending && context.status.state !== Succeeded) {
+            menuItems.push({
+                title: "Queue release",
+                action: function (actionContext) {
+                    alert("Execute action for status " + context.status.context.name);
+                },
+                disabled: false,
+                childItems: null,
+                icon: "css://bowtie-icon bowtie-status-run-outline"
+            });
+        }
+
+        if (context.status.state === Pending) {
+            menuItems.push({
+                title: "Restart release",
+                action: function (actionContext) {
+                    alert("Execute action for status " + context.status.context.name);
+                },
+                icon: "css://bowtie-icon bowtie-status-run-outline"
+            });
+        }
 
         return menuItems;
     }
